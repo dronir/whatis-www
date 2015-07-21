6ec9c1b0378ba4a601707d44b8286496f711d22d
Olli Wilkman <olli.wilkman@iki.fi>
2015-07-21 23:44:19 +0300

    Database stuff in code

--- whatis.py
+++ whatis.py
@@ -1,22 +1,33 @@
 # Encoding: utf8
 
 from __future__ import unicode_literals, print_function, division
-from flask import Flask
-
-DEFS = {
-    "dronir" : "awesome",
-    "adom" : "Dunno lol",
-    "sin" : "see: Nethack"
-}
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, flash
+from flask import render_template
+from contextlib import closing
 
 app = Flask(__name__)
+app.config.from_object("config")
 
-@app.route('/<thing>')
-def what(thing=None):
-    if thing and thing in DEFS.keys():
-        return "{}: {}".format(thing, DEFS[thing])
-    else:
-        return "Don't know about '{}'...".format(thing)
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
