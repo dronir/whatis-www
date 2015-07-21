42ee90f84e4ee840c129612e1931ec2c0504a461
Olli Wilkman <olli.wilkman@iki.fi>
2015-07-21 22:00:51 +0300

    Dict test

--- whatis.py
+++ whatis.py
@@ -3,11 +3,20 @@
 from __future__ import unicode_literals, print_function, division
 from flask import Flask
 
+DEFS = {
+    "dronir" : "awesome",
+    "adom" : "Dunno lol",
+    "sin" : "see: Nethack"
+}
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Lol hello."
+@app.route('/<thing>')
+def what(thing=None):
+    if thing and thing in DEFS.keys():
+        return "{}: {}".format(thing, DEFS[thing])
+    else:
+        return "Don't know about '{}'...".format(thing)
 
 if __name__ == "__main__":
     app.run()
